21df4b379ba1ebbde691dffec1d69ded217e5e16
asdasda3456 <contact.minceraftyt@gmail.com>
2024-12-20 12:44:53 +0200

    syntax

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+        if (argc < 2) {
+                printf("kot: no input files\n");
+                return -1;
+        }
+        else {
+                return 0;
+        }
+
+        return 0;
+}
